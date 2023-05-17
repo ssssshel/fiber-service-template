cd5bbc5928987e5108f9a8f4a3d5eec1e7ec8055
ssssshel <angelarteaga989@gmail.com>
2023-05-17 16:43:49 -0500

    main mod

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/ssssshel/fiber-service-template/src/middlewares"
+	"github.com/ssssshel/fiber-service-template/src/shared/config"
 )
 
 func main() {
@@ -25,4 +28,7 @@ func defaultInitConf(app *fiber.App, tokenization bool) {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("MS TEMPLATE")
 	})
+
+	fmt.Printf("Server running on port %s\n", config.Port())
+	app.Listen(":" + config.Port())
 }
